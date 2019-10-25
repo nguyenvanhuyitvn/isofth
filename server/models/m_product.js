215350005bf535cb30be8f474267252276959d84
nguyenvanhuyitvn <47744458+leonard9x@users.noreply.github.com>
2019-10-25 09:08:26 +0700

    V6
    update m_product

--- server/models/m_product.js
+++ server/models/m_product.js
@@ -8,71 +8,396 @@ module.exports = (sequelize, SequelizeDataTypes) => {
             primaryKey: true,
             type: DataTypes.INTEGER
         },
-        ad_client_id: DataTypes.INTEGER,
-        ad_org_id: DataTypes.INTEGER,
-        isactive: DataTypes.STRING,
-        createdby: DataTypes.INTEGER,
-        updatedby: DataTypes.INTEGER,
-        value: DataTypes.STRING,
-        name: DataTypes.STRING,
-        description: DataTypes.STRING,
-        documentnote: DataTypes.STRING,
-        help: DataTypes.STRING,
-        upc: DataTypes.STRING,
-        sku: DataTypes.STRING,
-        c_uom_id: DataTypes.INTEGER,
-        salesrep_id: DataTypes.INTEGER,
-        issummary: DataTypes.STRING,
-        isstocked: DataTypes.STRING,
-        ispurchased: DataTypes.STRING,
-        issold: DataTypes.STRING,
-        isbom: DataTypes.STRING,
-        isinvoiceprintdetails: DataTypes.STRING,
-        ispicklistprintdetails: DataTypes.STRING,
-        isverified: DataTypes.STRING,
-        c_revenuerecognition_id: DataTypes.INTEGER,
-        m_product_category_id: DataTypes.INTEGER,
-        classification: DataTypes.STRING,
-        volume: DataTypes.INTEGER,
-        weight: DataTypes.INTEGER,
-        shelfwidth: DataTypes.INTEGER,
-        shelfheight: DataTypes.INTEGER,
-        shelfdepth: DataTypes.INTEGER,
-        unitsperpallet: DataTypes.INTEGER,
-        c_taxcategory_id: DataTypes.INTEGER,
-        s_resource_id: DataTypes.INTEGER,
-        discontinued: DataTypes.STRING,
-        processing: DataTypes.STRING,
-        s_expensetype_id: DataTypes.INTEGER,
-        producttype: DataTypes.STRING,
-        imageurl: DataTypes.STRING,
-        descriptionurl: DataTypes.STRING,
-        guaranteedays: DataTypes.INTEGER,
-        r_mailtext_id: DataTypes.INTEGER,
-        versionno: DataTypes.STRING,
-        m_attributeset_id: DataTypes.INTEGER,
-        m_attributesetinstance_id: DataTypes.INTEGER,
-        downloadurl: DataTypes.STRING,
-        m_freightcategory_id: DataTypes.INTEGER,
-        m_locator_id: DataTypes.INTEGER,
-        guaranteedaysmin: DataTypes.INTEGER,
-        iswebstorefeatured: DataTypes.STRING,
-        isselfservice: DataTypes.STRING,
-        c_subscriptiontype_id: DataTypes.STRING,
-        isdropship: DataTypes.STRING,
-        isexcludeautodelivery: DataTypes.STRING,
-        group1: DataTypes.STRING,
-        group2: DataTypes.STRING,
-        istoformule: DataTypes.STRING,
-        lowlevel: DataTypes.INTEGER,
-        unitsperpack: DataTypes.INTEGER,
-        copyfrom: DataTypes.STRING,
-        m_product_uu: DataTypes.STRING,
-        m_parttype_id: DataTypes.INTEGER,
-        iskanban: DataTypes.STRING,
-        ismanufactured: DataTypes.STRING,
-        isphantom: DataTypes.STRING,
-        isownbox: DataTypes.STRING,
+        ad_client_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        ad_org_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        isactive: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        createdby: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        updatedby: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        value: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        documentnote: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        help: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        upc: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        sku: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        c_uom_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        salesrep_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        issummary: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isstocked: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        ispurchased: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        issold: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isbom: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isinvoiceprintdetails: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        ispicklistprintdetails: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isverified: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        c_revenuerecognition_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        m_product_category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        classification: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        volume: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        shelfwidth: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        shelfheight: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        shelfdepth: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        unitsperpallet: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        c_taxcategory_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        s_resource_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        discontinued: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        processing: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        s_expensetype_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        producttype: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        imageurl: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        descriptionurl: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        guaranteedays: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        r_mailtext_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        versionno: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        m_attributeset_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        m_attributesetinstance_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        downloadurl: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        m_freightcategory_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        m_locator_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        guaranteedaysmin: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        iswebstorefeatured: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isselfservice: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        c_subscriptiontype_id: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isdropship: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isexcludeautodelivery: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        group1: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        group2: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        istoformule: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        lowlevel: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        unitsperpack: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        copyfrom: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        m_product_uu: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        m_parttype_id: {
+            type: DataTypes.INTEGER,
+            allowNull: {
+                args: false
+            }
+        },
+        iskanban: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        ismanufactured: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isphantom: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
+        isownbox: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false
+            }
+        },
         createdAt: {
             field: 'created',
             type: DataTypes.DATE_NO_TZ
@@ -80,13 +405,7 @@ module.exports = (sequelize, SequelizeDataTypes) => {
         updatedAt: {
             field: 'updated',
             type: DataTypes.DATE_NO_TZ
-        },
-        discontinuedby: {
-            type: DataTypes.DATE_NO_TZ
-        },
-        discontinuedat: {
-            type: DataTypes.DATE_NO_TZ,
-        },
+        }
     }, { timestamp: true });
     m_product.associate = function(models) {
         // associations can be defined here
